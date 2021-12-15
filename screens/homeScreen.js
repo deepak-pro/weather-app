1bd023c52ff6cae830399bae3ab7b124c0c4836d
Deepak <deepak@Deepaks-MacBook-Pro.local>
2021-12-15 09:56:42 +0530

    Add background

--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -19,47 +19,35 @@ import { API_KEY } from "../constants";
 
 const image = require("../assets/background.png");
 const link =
-  "https://api.openweathermap.org/data/2.5/weather?q=indore&appid=27506ebae89d5ad0189eaa55b3215086";
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=indore&appid=27506ebae89d5ad0189eaa55b3215086";
 
 function getWeatherData() {
   return fetch(link).then((data) => data.json());
 }
 
-function generateGridData(data) {
-  let gridData = [];
-  gridData.push(data.wind.speed);
-  gridData.push(data.main.pressure);
-  gridData.push(data.main.humidity);
-  gridData.push(data.visibility + " m");
-  gridData.push(data.main.feels_like);
-  gridData.push(data.clouds.all);
-  return gridData;
-}
-
 export default function HomeScreen() {
-  const [gdata, setGdata] = useState([]);
   const [data, setData] = useState({
     coord: { lon: 75.8333, lat: 22.7179 },
     weather: [{ id: 721, main: "Haze", description: "haze", icon: "50d" }],
     base: "stations",
     main: {
-      temp: 298.25,
-      feels_like: 298.04,
-      temp_min: 298.25,
-      temp_max: 298.25,
-      pressure: 1017,
-      humidity: 47,
+      temp: 16.1,
+      feels_like: 16.06,
+      temp_min: 16.1,
+      temp_max: 16.1,
+      pressure: 1019,
+      humidity: 88,
     },
-    visibility: 5000,
-    wind: { speed: 2.57, deg: 200 },
-    clouds: { all: 11 },
-    dt: 1639466954,
+    visibility: 1000,
+    wind: { speed: 0, deg: 0 },
+    clouds: { all: 40 },
+    dt: 1639540766,
     sys: {
       type: 1,
       id: 9067,
       country: "IN",
-      sunrise: 1639445325,
-      sunset: 1639484038,
+      sunrise: 1639531760,
+      sunset: 1639570459,
     },
     timezone: 19800,
     id: 1269743,
@@ -70,74 +58,89 @@ export default function HomeScreen() {
   useEffect(() => {
     console.log("I am here");
     //getWeatherData().then((data) => setData(data));
-    const gridData = generateGridData(data);
-    setGdata(gridData);
   }, []);
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={styles.upperview}>
-        <Text style={{ fontSize: 30 }}>{data.name}</Text>
-        <Text style={{ fontSize: 60 }}> {data.main.temp}&deg;</Text>
-        <Image
-          source={{
-            uri: "https://openweathermap.org/img/wn/50d@2x.png",
-          }}
-          style={{ height: 50, width: 50 }}
-          resizeMode="cover"
-        />
-        <Text style={{ fontSize: 20 }}>{data.weather[0].main}</Text>
-        <Text style={{ fontSize: 15, margin: 10 }}>
-          High: {data.main.temp_max}&deg; {data.main.temp_min}: 14&deg;
-        </Text>
-      </View>
-      <View style={styles.lowerview}>
-        {/* <FlatGrid
-          style={styles.gridView}
-          itemDimension={130}
-          data={gdata}
-          spacing={10}
-          renderItem={({ item }) => (
-            <View style={styles.itemContainer}>
-              <Text style={{ fontSize: 30 }}>{item}</Text>
-            </View>
-          )}
-        /> */}
-
-        <ScrollView>
-          <View style={styles.lowerrow}>
-            <View style={styles.loweritem}>
-              <Text style={styles.lowerviewtext}> Wind Speed </Text>
-              <Text style={styles.lowerviewtext}> {data.wind.speed} </Text>
-            </View>
-            <View style={styles.loweritem}>
-              <Text style={styles.lowerviewtext}> Pressure </Text>
-              <Text style={styles.lowerviewtext}> {data.main.pressure} </Text>
-            </View>
+    <View style={{ flex: 1 }}>
+      <ImageBackground
+        source={require("../assets/background.png")}
+        style={{ flex: 1 }}
+      >
+        <SafeAreaView style={{ flex: 1 }}>
+          <View style={styles.upperview}>
+            <Text style={[{ fontSize: 30 }, styles.whitetext]}>
+              {data.name}
+            </Text>
+            <Text style={[{ fontSize: 60 }, styles.whitetext]}>
+              {" "}
+              {data.main.temp}&deg;
+            </Text>
+            <Image
+              source={{
+                uri: "https://openweathermap.org/img/wn/" + "03d" + "@2x.png",
+              }}
+              style={{ height: 50, width: 50 }}
+              resizeMode="cover"
+            />
+            <Text style={[{ fontSize: 20 }, styles.whitetext]}>
+              {data.weather[0].main}
+            </Text>
+            <Text style={[{ fontSize: 15, margin: 10 }, styles.whitetext]}>
+              High: {data.main.temp_max}&deg; Low: {data.main.temp_min}&deg;
+            </Text>
           </View>
-          <View style={styles.lowerrow}>
-            <View style={styles.loweritem}>
-              <Text style={styles.lowerviewtext}> Humidity </Text>
-              <Text style={styles.lowerviewtext}> {data.main.humidity} </Text>
-            </View>
-            <View style={styles.loweritem}>
-              <Text style={styles.lowerviewtext}> Visibility </Text>
-              <Text style={styles.lowerviewtext}> {data.visibility} </Text>
-            </View>
+          <View style={styles.lowerview}>
+            <ScrollView>
+              <View style={styles.lowerrow}>
+                <View style={styles.loweritem}>
+                  <Text style={styles.lowerviewtext}> Wind Speed </Text>
+                  <Text style={styles.lowerviewtext}>
+                    {" "}
+                    {data.wind.speed} meter/sec{" "}
+                  </Text>
+                </View>
+                <View style={styles.loweritem}>
+                  <Text style={styles.lowerviewtext}> Pressure </Text>
+                  <Text style={styles.lowerviewtext}>
+                    {" "}
+                    {data.main.pressure} hPa{" "}
+                  </Text>
+                </View>
+              </View>
+              <View style={styles.lowerrow}>
+                <View style={styles.loweritem}>
+                  <Text style={styles.lowerviewtext}> Humidity </Text>
+                  <Text style={styles.lowerviewtext}>
+                    {" "}
+                    {data.main.humidity}%{" "}
+                  </Text>
+                </View>
+                <View style={styles.loweritem}>
+                  <Text style={styles.lowerviewtext}> Visibility </Text>
+                  <Text style={styles.lowerviewtext}>
+                    {" "}
+                    {data.visibility} meter{" "}
+                  </Text>
+                </View>
+              </View>
+              <View style={styles.lowerrow}>
+                <View style={styles.loweritem}>
+                  <Text style={styles.lowerviewtext}> Feels Like </Text>
+                  <Text style={styles.lowerviewtext}>
+                    {" "}
+                    {data.main.feels_like}&deg;{" "}
+                  </Text>
+                </View>
+                <View style={styles.loweritem}>
+                  <Text style={styles.lowerviewtext}> Cloudiness </Text>
+                  <Text style={styles.lowerviewtext}> {data.clouds.all}% </Text>
+                </View>
+              </View>
+            </ScrollView>
           </View>
-          <View style={styles.lowerrow}>
-            <View style={styles.loweritem}>
-              <Text style={styles.lowerviewtext}> Feels Like </Text>
-              <Text style={styles.lowerviewtext}> {data.main.feels_like} </Text>
-            </View>
-            <View style={styles.loweritem}>
-              <Text style={styles.lowerviewtext}> Clouds </Text>
-              <Text style={styles.lowerviewtext}> {data.clouds.all} </Text>
-            </View>
-          </View>
-        </ScrollView>
-      </View>
-    </SafeAreaView>
+        </SafeAreaView>
+      </ImageBackground>
+    </View>
   );
 }
 
@@ -150,13 +153,13 @@ const styles = StyleSheet.create({
     paddingTop: StatusBar.currentHeight,
   },
   upperview: {
-    backgroundColor: "white",
+    backgroundColor: "transparent",
     flex: 0.4,
     alignItems: "center",
-    justifyContent: "flex-end",
+    justifyContent: "center",
   },
   lowerview: {
-    backgroundColor: "white",
+    backgroundColor: "transparent",
     flex: 0.6,
     alignItems: "center",
     justifyContent: "flex-start",
@@ -164,6 +167,9 @@ const styles = StyleSheet.create({
   lowerrow: {
     flexDirection: "row",
     margin: 10,
+  },
+  whitetext: {
+    color: "white",
   },
   loweritem: {
     width: Dimensions.get("window").width / 2 - 30,
